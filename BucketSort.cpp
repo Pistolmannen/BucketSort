f0fdefa8e2dddc9f783c260b97aec69f894cc0a2
a23erigu <a23erigu@student.his.se>
2024-11-21 16:07:39 +0100

    V now gets sorted and outputted

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -1,16 +1,28 @@
-// C++ Program to initializ std::vector with
-// initializer list
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
+    // Initializing std::vector with list of multiple values
+    vector<int> v = {1, 4, 5, 3, 2};
 
-    // Initializing std::vector with list of
-    // multiple values
-    vector<int> v = {11, 23, 45, 89};
+    // Finding the maximum element in the vector v
+    int k = *max_element(v.begin(), v.end());
 
+    // Creating a vector w with size k + 1
+    vector<int> w(k + 1);
+
+    // Output the elements of vector v
+    cout << "Elements of v in a sorted order: ";
     for (auto i : v)
-        cout << i << " ";
+        w[i] = i;
+
+    for (auto i : w)
+        if (i != 0){
+            cout << i << " ";
+        }
+    
     return 0;
 }
