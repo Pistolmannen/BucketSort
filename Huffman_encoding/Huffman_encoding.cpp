88b14f09b37dbc05d7c56f8eed6ee7182a01b49d
a23erigu <a23erigu@student.his.se>
2024-12-04 15:50:47 +0100

    trying to fix stuff

--- Huffman_encoding/Huffman_encoding.cpp
+++ Huffman_encoding/Huffman_encoding.cpp
@@ -4,20 +4,20 @@
 
 using namespace std;
 
-class Tree {    // Tree class
+class Tree {                                // Tree class
 public:
-    Tree(int w, char c){    // Creates a tree with a character
+    Tree(int w, char c){                    // Creates a tree with a character
         weight = w;
         character = c;
     };
 
-    Tree(int w, Tree* t1, Tree* t2){  // Creates a tree with two branches
+    Tree(int w, Tree* t1, Tree* t2){        // Creates a tree with two branches
         weight = w;
         right = t1;
         left = t2;
     };
 
-    ~Tree(){                    // Deletes the root node of the tree
+    ~Tree(){                                // Deletes the root node of the tree
         if (left != NULL){
             free(left);
         }
@@ -26,8 +26,8 @@ public:
         }
     };
 
-    int getWeight() {         // Returns the weight of the tree 
-        return(weight);
+    int getWeight() {          
+        return(weight);                     // Returns the weight of the tree
     };
 
     string printTree(vector<char>& bitString) {   // Print the components of the tree 
@@ -35,41 +35,37 @@ public:
             
             for (char bit : bitString) {        // Adds the bit path so it can be printed out
                 numberBit.push_back(bit);
-                testBit.push_back(bit);
+                encodedBitString.push_back(bit);
                 numberBit.append(" ");
             }
 
-            cout << numberBit << " : " << character << endl;
+            cout << numberBit << ": " << character << endl;
 
-            for (int i = 0; i < weight; i++){
-                fullBit.append(testBit);
+            for (int i = 0; i < weight; i++){ // Adds the correct bit(s) per character into the output string
+                fullBit.append(encodedBitString);
             }
-            return(fullBit);
-
+            return(fullBit);                // Returns the output string
         }
         else{                               // Continues down the tree
-
-            if(right->getWeight() >= left->getWeight()){
+            if(right->getWeight() >= left->getWeight()){ // Takes the right side if it has a higher or equal weight to the left side
                 bitString.push_back('0');
-                fullBit = (right->printTree(bitString));
+                fullBit.append(right->printTree(bitString));
 
                 bitString.pop_back();
 
                 bitString.push_back('1');
                 fullBit.append(left->printTree(bitString));
             }
-            else{
+            else{                           // Takes the left side otherwise
                 bitString.push_back('1');
-                fullBit = (left->printTree(bitString));
+                fullBit.append(left->printTree(bitString));
                 
                 bitString.pop_back();
 
                 bitString.push_back('0');
                 fullBit.append(right->printTree(bitString));
             }
-            
-
-            return(fullBit);
+            return(fullBit);                 // Returns the output string
         }
     }; 
 
@@ -77,18 +73,16 @@ private:
     Tree* left;
     Tree* right;
     int weight;
-    int rightWeight;
-    int leftWeight;
     char character;
     string numberBit;
-    string testBit;
+    string encodedBitString;
     string fullBit;
 };
 
 #ifndef NULL
 #define NULL 0
 #endif
-struct TreeWrapper {        // Tree-helper
+struct TreeWrapper {                           // Tree-helper
     TreeWrapper() {
         tree = NULL;
     }
@@ -103,20 +97,33 @@ struct TreeWrapper {        // Tree-helper
 
 int main(int argc, char const *argv[])
 {
-    priority_queue<TreeWrapper> queue;          // Creates the bottoms of the tree
-    queue.push(TreeWrapper(new Tree(4, 'a')));
+    priority_queue<TreeWrapper> queue;          // Creates the bottom of the tree
+    
     queue.push(TreeWrapper(new Tree(3, 'b')));
     queue.push(TreeWrapper(new Tree(1, 'c')));
+    queue.push(TreeWrapper(new Tree(2, 'j')));
+    queue.push(TreeWrapper(new Tree(7, 'l')));
+    queue.push(TreeWrapper(new Tree(12, 'a')));
 
-    while (queue.size() > 1)            // Puts the entire tree together
+    /*
+    while (queue.size() > 0) {
+        TreeWrapper test = queue.top();
+        queue.pop();
+        cout << test.tree->getWeight() << endl;
+    }
+    */
+
+    while (queue.size() > 1)                    // Puts the entire tree together
     {
         TreeWrapper tree1 = queue.top();
         queue.pop();
         TreeWrapper tree2 = queue.top();
         queue.pop();
-        int treeWeight = tree1.tree->getWeight() + tree2.tree->getWeight();
+        int treeWeight = (tree1.tree->getWeight() + tree2.tree->getWeight());
 
-        queue.push(TreeWrapper(new Tree(treeWeight, tree1.tree, tree2.tree)));
+        cout << tree1.tree->getWeight() << " plus " << tree2.tree->getWeight() << " make " << treeWeight << endl;
+
+        queue.push(TreeWrapper(new Tree(treeWeight, tree1.tree, tree2.tree)));  // make werd shit cuse priorety :)
     }
 
     TreeWrapper fullTreeWrapper = queue.top();      // Retreives the fully built tree
